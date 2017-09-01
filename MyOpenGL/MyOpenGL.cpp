03dfe5abf7e203f877f553ab7179a003d02389ed
CaffeineCreature <chrispearson9@gmail.com>
2017-09-01 18:19:40 -0500

    Compatibility and stability changes.
    Compatibility and stability changes.

--- MyOpenGL/MyOpenGL.cpp
+++ MyOpenGL/MyOpenGL.cpp
@@ -1,2 +1,82 @@
+/**
+
+Copyright 2010 Etay Meiri
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/**/
+
 // MyOpenGL.cpp : Defines the entry point for the console application.
 // Folowing http://ogldev.atspace.co.uk/
+
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include "ogldev_math_3d.h"
+
+GLuint VBO;
+
+static void RenderSceneCB()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glDrawArrays(GL_POINTS, 0, 1);
+
+	glDisableVertexAttribArray(0);
+
+	glutSwapBuffers();
+}
+
+static void InitializeGlutCallbacks()
+{
+	glutDisplayFunc(RenderSceneCB);
+}
+
+static void CreateVertexBuffer()
+{
+	Vector3f Vertices[1];
+	Vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(1024, 768);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Toutorial");
+
+	InitializeGlutCallbacks();
+
+	// Check AFTER GLUT Initialization
+	GLenum res = glewInit();
+	if (res != GLEW_OK) {
+		fprintf(stderr, "ERROR: '%s'\n", glewGetErrorString(res));
+	}
+
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	CreateVertexBuffer();
+
+	glutMainLoop();
+
+	return 0;
+}
